77e76849388eb3916e9ac5aa43edbf66c00a77fe
mohongen <496585582@qq.com>
2024-09-27 10:25:50 +0800

    feat: test esbuild

---
+++ src/components/MacroTest.vue
@@ -0,0 +1,7 @@
+<script setup lang="tsx">
+const props = withDefaults(defineProps<{}>(), {});
+defineRender(() => {
+  return <div>content</div>;
+});
+</script>
+<style lang="scss" scoped></style>
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from './components/Button.vue';
+// export { default as MacroTest } from './components/MacroTest.vue';
 // export { default as Input } from './components/ButtonTsx.vue';
